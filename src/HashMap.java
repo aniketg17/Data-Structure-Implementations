782107cf01fd3bb7038a5c127a232ac3e3c66898
aniketg17 <aniketkg@gmail.com>
2020-06-08 19:46:01 +0530

    Implemented remove(), decreaseArraySize(), containsKey(), containsValue()

--- src/HashMap.java
+++ src/HashMap.java
@@ -5,6 +5,7 @@ public class HashMap<Key, Value> implements HashMapInterface{
     private int numberOfSlots;
     private final int INITIAL_ARRAY_SIZE = 13;
     private int currentArraySize;
+    private final double MAX_LOAD = 0.75;
 
 
     private static class HashNode<Key, Value> {
@@ -45,11 +46,11 @@ public class HashMap<Key, Value> implements HashMapInterface{
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
     public Value put(Key k, Value v) {
-        if (getLoadFactor() > 0.75) {
+        if (getLoadFactor() > MAX_LOAD) {
             increaseArraySize();
         }
         int hashcode = getHashCode(k);
@@ -58,11 +59,8 @@ public class HashMap<Key, Value> implements HashMapInterface{
         if (array[hashcode] == null) {
             array[hashcode] = insert;
         } else {
-            HashNode node = array[hashcode];
-            while (node.next != null) {
-                node = node.next;
-            }
-            node.next = insert;
+            insert.next = array[hashcode];
+            array[hashcode] = insert;
         }
         ++size;
         ++numberOfKeys;
@@ -85,7 +83,37 @@ public class HashMap<Key, Value> implements HashMapInterface{
     }
 
     public Value remove(Key k) {
-        return null;
+        int hash = getHashCode(k);
+        Value value = null;
+        HashNode<Key, Value> prev = null;
+        for (HashNode<Key, Value> node = array[hash]; node != null; node = node.next) {
+            if (node.key.equals(k)) {
+                if (prev == null) {
+                    value = node.value;
+                    array[hash] = array[hash].next;
+                    return value;
+                } else {
+                    value = prev.next.value;
+                    prev.next = node.next;
+                }
+            } else {
+                prev = node;
+            }
+        }
+        --numberOfKeys;
+
+        if (getLoadFactor() <= 0.25) {
+            decreaseArraySize();
+        }
+
+        return value;
+    }
+
+    private void decreaseArraySize() {
+        HashNode<Key, Value>[] newArray = new HashNode[(int) (0.5 * currentArraySize)];
+        System.arraycopy(array, 0, newArray, 0, array.length);
+        array = newArray;
+        numberOfSlots = array.length;
     }
 
     public Value get(Object k) {
@@ -96,8 +124,42 @@ public class HashMap<Key, Value> implements HashMapInterface{
         return null;
     }
 
+    public boolean containsKey(Key k) {
+        int hash = getHashCode(k);
+        for (HashNode<Key, Value> node = array[hash]; node != null; node = node.next) {
+            if (node.key.equals(k)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean containsValue(Value v) {
+        for (HashNode<Key, Value> node : array) {
+            if (node != null) {
+                for (HashNode<Key, Value> trav = node; trav != null; trav = trav.next) {
+                    if (trav.value.equals(v)) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
     @Override
     public int size() {
-        return 0;
+        return size;
+    }
+
+    public static void main(String[] args) {
+        HashMap<Integer, Integer> hash = new HashMap<>();
+        System.out.println(hash.isEmpty());
+        hash.put(2, 4);
+        hash.put(4,1);
+        //hash.put(4,1);
+        hash.put(15,1);
+
+
     }
 }
