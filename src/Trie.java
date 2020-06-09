3e44f6a1a97be275e06245bee0a09d8a7b69247a
aniketg17 <aniketkg@gmail.com>
2020-06-09 20:32:42 +0530

    Insert method in Trie complete

--- src/Trie.java
+++ src/Trie.java
@@ -1,16 +1,39 @@
 public class Trie<Value> {
 
-    private final int MAX_INDICES = 256; // change depending on need. currently suited for ASCII
+    private static final int MAX_INDICES = 256; // change depending on need. currently suited for ASCII
     private TrieNode root;
     private int size;
 
-    private class TrieNode {
-        Value v;
-        Trie[] next = new Trie[MAX_INDICES];
+    private static class TrieNode {
+        private Object val;
+        private TrieNode[] next = new TrieNode[MAX_INDICES];
     }
 
     public Trie() {
         this.root = null;
     }
 
+    public Value insert(String key, Value value) {
+        root = insert(root, key, value, 0);
+        return value;
+    }
+
+    public TrieNode insert(TrieNode root, String key, Value value, int charCount) {
+        if (root == null) {
+            root = new TrieNode();
+        }
+        if (charCount == key.length()) {
+            root.val = value;
+            return root;
+        }
+        char insert = key.charAt(charCount);
+        root.next[insert] = insert(root.next[insert], key, value, charCount + 1);
+        return root;
+    }
+
+    public static void main(String[] args) {
+        Trie<Integer> bsdk = new Trie<>();
+        bsdk.insert("Aniket", 1);
+        bsdk.insert("Ani", 3);
+    }
 }
